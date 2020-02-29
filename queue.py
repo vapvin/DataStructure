058d3ef521abd32e2383a78706c2444781b03452
vapvin <tpehdj12@daum.net>
2020-02-29 16:18:46 +0900

    Editing

---
+++ queue.py
@@ -0,0 +1,3 @@
+# Queue is a linear data structure where the first element is inserted from one end called REAR and deleted from the other end called as FRONT.
+# 큐는 REAR이라는 한 쪽 끝에서 첫 번째 요소를 삽입하고 다른 쪽 끝에서 FRONT라고 불리는 다른 쪽 끝에서 삭제하는 선형 데이터 구조다.
+
