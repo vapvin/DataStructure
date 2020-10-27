629147537905ccf8d9d0d27e2fa0220903ab9dae
vapvin <tpehdj12@daum.net>
2020-10-27 21:46:07 +0900

    Inital Commit

--- linkeList.js
+++ linkeList.js
@@ -3,4 +3,13 @@ class Node {
         this.data = data;
         this.next = null;
     }
+
+    add() {
+
+    }
 }
+
+const node1 = new Node(2);
+const node2 = new Node(4);
+node1.next = node2;
+node2.next = null;
